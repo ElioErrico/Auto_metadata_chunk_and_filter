175225f864b91630e64ead8b42955324e991dd89
ElioErrico <143315119+ElioErrico@users.noreply.github.com>
2024-05-09 17:07:05 +0200

    Update my_plugin.py

--- my_plugin.py
+++ my_plugin.py
@@ -1,31 +1,48 @@
-from cat.mad_hatter.decorators import tool, hook, plugin
-from pydantic import BaseModel
-from datetime import datetime, date
+from cat.mad_hatter.decorators import hook
+import os
 
-class MySettings(BaseModel):
-    required_int: int
-    optional_int: int = 69
-    required_str: str
-    optional_str: str = "meow"
-    required_date: date
-    optional_date: date = 1679616000
+def stampa(testo, nome_file, percorso="C:\\Users\\elio.errico\\Desktop\\python\\cheshire-cat-main\\core-main\\core"):
+    
+    percorso_completo = os.path.join(percorso, nome_file)
+    
+    # Assicurarsi che la cartella esista, altrimenti crearla
+    if not os.path.exists(percorso):
+        os.makedirs(percorso)
+    
+    # Scrivere il testo nel file
+    with open(percorso_completo, 'w', encoding='utf-8') as file:
+        file.write(testo)  
 
-@plugin
-def settings_model():
-    return MySettings
+    return 
 
-@tool
-def get_the_day(tool_input, cat):
-    """Get the day of the week. Input is always None."""
 
-    dt = datetime.now()
 
-    return dt.strftime('%A')
+@hook  # default priority = 1
+def before_rabbithole_splits_text(doc, cat):
 
-@hook
-def before_cat_sends_message(message, cat):
+    # nome_file="01_before_rabbithole_splits_text.txt"
+    nome_file=str(doc[0].metadata['source'])
+    content = str(doc[0].page_content)
 
-    prompt = f'Rephrase the following sentence in a grumpy way: {message["content"]}'
-    message["content"] = cat.llm(prompt)
 
-    return message
+    title=cat.llm(f"Trova un titolo conciso di 4 parole che descriva al meglio il seguente testo:\n {content}")
+    doc[0].metadata["title"]=title
+    metadata =str(doc[0].metadata)
+    # titolo = "titolo= " + str(doc[0].metadata['title'])
+    # testo=  content + "\n \n \n \n \n \n" + metadata + "\n \n \n \n \n \n" + titolo
+    # stampa(testo,nome_file)
+
+    return doc
+
+
+@hook  # default priority = 1
+def after_rabbithole_splitted_text(chunks, cat):
+
+    for chunk in chunks:
+        titolo_chunk =str(chunk.metadata['title'])
+        testo_chunk=str(chunk.page_content)
+        testo_chunk_con_titolo=titolo_chunk+":\n"+testo_chunk
+
+        chunk.page_content=testo_chunk_con_titolo
+
+    return chunks
