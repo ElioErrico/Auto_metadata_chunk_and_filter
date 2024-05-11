553033eecae4e29c6ef2f311778a64b99d8c8dd2
ElioErrico <143315119+ElioErrico@users.noreply.github.com>
2024-05-11 17:02:52 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ from cat.mad_hatter.decorators import plugin
 
 
 class MySettings(BaseModel):
-    text_to_search_the_title: str = "Individua un titolo conciso di 4 parole che descriva al meglio il seguente testo. Per individuare il titolo utilizza le seguenti regole:"
-    rule_1: str  = "1) Se nel testo è citato un errore, includi nel titolo il numero dell'errore"
-    rule_2: str  = "2) Il titolo deve includere le parole chiave contenute nel testo"
-    rule_3: str  = ""
+    text_to_search_the_title: str = "Concentrati e individua un titolo conciso di 4 parole che descriva al meglio il seguente testo. Per individuare il titolo utilizza le seguenti regole:"
+    rule_1: str  = "1) Se nel testo è citato un errore, includi nel titolo il numero dell'errore (Non confondere il numero)""
+    rule_2: str  = "2) Se nel testo non è citato alcun errore non includere il numero dell'errore"
+    rule_3: str  = "3) Il titolo deve includere le parole chiave contenute nel testo"
     n_of_chunk_for_one_title: int = 5
     prompt_for_question_generation: str= "Genera 2 domande molto concise la cui risposta \u00e8 contenuta nel seguente testo (per creare le domande fai riferimento al titolo):"
 
