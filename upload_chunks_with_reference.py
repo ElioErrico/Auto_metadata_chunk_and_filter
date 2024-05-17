ddbf92f2e2883b2baebcc782c06991fcb449b5f0
ElioErrico <143315119+ElioErrico@users.noreply.github.com>
2024-05-17 16:44:45 +0200

    Update upload_chunks_with_reference.py

--- upload_chunks_with_reference.py
+++ upload_chunks_with_reference.py
@@ -1,11 +1,10 @@
-
 from cat.mad_hatter.decorators import hook
 from langchain.docstore.document import Document
 import os
 import json
 
 
-def save_list_to_json(data_list, filename, path="list_of_titles"):
+def save_list_to_json(data_list, filename, path):
     # Assicurati che il percorso esista, altrimenti crealo
     if not os.path.exists(path):
         os.makedirs(path)
@@ -13,21 +12,11 @@ def save_list_to_json(data_list, filename, path="list_of_titles"):
     # Crea il percorso completo del file
     full_path = os.path.join(path, filename)
     
-    # Leggi i dati esistenti se il file esiste
-    if os.path.exists(full_path):
-        with open(full_path, 'r') as file:
-            existing_data = json.load(file)
-        # Aggiungi i nuovi dati alla lista esistente
-        existing_data.extend(data_list)
-    else:
-        # Altrimenti, usa la nuova lista come dati da salvare
-        existing_data = data_list
-    
     # Scrivi la lista aggiornata nel file JSON
     with open(full_path, 'w') as file:
-        json.dump(existing_data, file)
+        json.dump(data_list, file)
 
-def read_list_from_json(filename, path="list_of_titles"):
+def read_list_from_json(filename, path):
     # Crea il percorso completo del file
     full_path = os.path.join(path, filename)
     
@@ -45,7 +34,7 @@ def read_list_from_json(filename, path="list_of_titles"):
 
     return data_list 
 
-def stampa(testo, nome_file, percorso="list_of_titles"):
+def stampa(testo, nome_file, path):
     """
     Writes or appends text to a text file. If the file does not exist, creates it with the text as initial content.
     If the file exists, appends the text to the existing content.
@@ -55,11 +44,11 @@ def stampa(testo, nome_file, percorso="list_of_titles"):
     nome_file (str): The name of the file.
     percorso (str): The directory path.
     """
-    percorso_completo = os.path.join(percorso, nome_file)
+    percorso_completo = os.path.join(path, nome_file)
     
     # Ensure the directory exists, otherwise create it
-    if not os.path.exists(percorso):
-        os.makedirs(percorso)
+    if not os.path.exists(path):
+        os.makedirs(path)
     
     # Determine the mode based on the existence of the file: append if it exists, write if not
     mode = 'a' if os.path.exists(percorso_completo) else 'w'
@@ -68,18 +57,25 @@ def stampa(testo, nome_file, percorso="list_of_titles"):
     with open(percorso_completo, mode, encoding='utf-8') as file:
         file.write(testo + "\n")  # Append new text with a newline for readability
 
+def get_current_directory():
+    """
+    Restituisce il percorso della cartella in cui è eseguito il file Python in esecuzione.
+    """
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    return current_directory
 
 @hook
 def after_cat_bootstrap(cat):
-    # call global parameter so can be called through out the various hooks
-    global list_of_titles
+
     # read from json file the possible tags
-    list_of_titles=read_list_from_json("list_of_tags.json")
+    directory=get_current_directory()
+    list_of_titles=read_list_from_json("list_of_tags.json",directory)
+    stampa(str(list_of_titles),"list_of_titles.txt",directory)
 
 @hook
 def before_cat_recalls_declarative_memories(declarative_recall_config, cat):
-    
-    global list_of_titles
+    directory=get_current_directory()
+    list_of_titles=read_list_from_json("list_of_tags.json",directory)
     # call chat history
     chat_history=cat.working_memory.history
     # let's classify the chat history based on json tags
@@ -93,7 +89,9 @@ def after_rabbithole_splitted_text(chunks, cat):
 
     #create list o concatenated chunks
     concatenated_chunks_list = []
-    global list_of_titles
+    directory=get_current_directory()
+    list_of_titles=read_list_from_json("list_of_tags.json",directory)
+    
 
     #load settings and set n° of chunk to aggregrate in order to find the correct tag of each chunk without forget the context
     settings = cat.mad_hatter.get_plugin().load_settings()
@@ -111,14 +109,13 @@ def after_rabbithole_splitted_text(chunks, cat):
         if create_tag_with_prompt==False:
             title = cat.classify(concatenated_content, labels=list_of_titles)
         else:
-            #TODO: Controllare questa parte
             search_for_title_prompt=settings["search_for_title_prompt"]
             title = cat.classify(concatenated_content, labels=list_of_titles)
-            if title=="nessuna classificazione":
+            if "nessuna classificazione" in title :
                 title = cat.llm(search_for_title_prompt +"\n"+ concatenated_content)
                 list_of_titles.append(title)
-
-            save_list_to_json(list_of_titles,"list_of_tags.json")
+                list_of_titles=list(set(list_of_titles))
+                save_list_to_json(list_of_titles,"list_of_tags.json",directory)
 
         # Generates metadata title for the new doc
         metadata_of_the_new_doc = {}
